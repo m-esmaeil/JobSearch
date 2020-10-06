75b642cb4966675857cc5f97540db2123afb8d1c
muhamed esmaeil <m_a_esmaeil@hotmail.com>
2020-10-06 08:35:47 +0200

    CRUD on Roles Controller

---
+++ JobSearch/Controllers/RolesController.cs
@@ -0,0 +1,105 @@
+﻿using JobSearch.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobSearch.Controllers
+{
+    public class RolesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            return View(db.Roles.ToList());
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(string id)
+        {
+            var role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(role);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Roles.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(string id)
+        {
+            var role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(string id)
+        {
+            var role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmDelete(string id)
+        {
+            var role2Del = db.Roles.Find(id);
+            if (role2Del == null)
+            {
+                return RedirectToAction("Index");
+            }
+            db.Roles.Remove(role2Del);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ JobSearch/Models/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSearch.Models
+{
+    public class Roles
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
